9f3505625d62c6aca363de67d58b79efb01de029
Gowthamsai2001 <89728756+Gowthamsai2001@users.noreply.github.com>
2021-10-28 14:45:49 +0530

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,10 @@
+import random
+while True:
+    def fun(a,b):
+        print(random.randint(a,b))
+    def cha(a,b):
+        x=random.randint(a,b)
+        print(chr(x))
+   fun(0,100)
+   cha(0,100)
+        
